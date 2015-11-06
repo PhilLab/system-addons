b0d5aba5f9b8364e4eec074029dcdbff5ecfe484
Robert Helmer <rhelmer@rhelmer.org>
2015-11-06 11:52:31 -0800

    use matching id

--- browser/extensions/loop/build_extension.sh
+++ browser/extensions/loop/build_extension.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ -f loop\@testing.mozilla.org.xpi ]; then
+if [ -f loop\@test.mozilla.org.xpi ]; then
   rm loop\@test.mozilla.org.xpi
 fi
 
-zip -x build_extension.sh -x standalone/\* -x build-jsx -x ui/ui-showcase.jsx run-all-loop-tests.sh -x test\* -r loop\@testing.mozilla.org.xpi .
+zip -x build_extension.sh -x standalone/\* -x build-jsx -x ui/ui-showcase.jsx run-all-loop-tests.sh -x test\* -r loop\@test.mozilla.org.xpi .
