80198d9c4a911229dd6a3dc6e525702472f678be
Robert Helmer <rhelmer@rhelmer.org>
2015-11-06 10:53:15 -0800

    missed this s/let/var/ in the rebase

--- browser/extensions/loop/build_extension.sh
+++ browser/extensions/loop/build_extension.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f loop\@testing.mozilla.org.xpi ]; then
-  rm loop\@testing.mozilla.org.xpi
+  rm loop\@test.mozilla.org.xpi
 fi
 
 zip -x build_extension.sh -x standalone/\* -x build-jsx -x ui/ui-showcase.jsx run-all-loop-tests.sh -x test\* -r loop\@testing.mozilla.org.xpi .
